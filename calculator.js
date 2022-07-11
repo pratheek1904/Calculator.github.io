2fa85404509ef5739a1207cda0e5dd1aa549341d
pratheek1904 <109038768+pratheek1904@users.noreply.github.com>
2022-07-11 23:02:05 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,26 @@
+function add(){
+    first=parseInt(myform.first.value);
+    second=parseInt(myform.second.value);
+    result=first+second;
+    myform.result.value=result;
+}
+function Sub(){
+    first=parseInt(myform.first.value);
+    second=parseInt(myform.second.value);
+    result=first-second;
+    myform.result.value=result;
+}
+
+function Mul(){
+    first=parseInt(myform.first.value);
+    second=parseInt(myform.second.value);
+    result=first*second;
+    myform.result.value=result;
+}
+
+function Div(){
+    first=parseInt(myform.first.value);
+    second=parseInt(myform.second.value);
+    result=first/second;
+    myform.result.value=result;
+}
